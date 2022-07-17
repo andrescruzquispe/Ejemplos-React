d819ccee12a94eaf230c289242c28fe31dc53ace
Andrés Cruz <100103222+andrescruzquispe@users.noreply.github.com>
2022-07-17 18:50:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
         <Testimonio
-          nombre="Shawn Wang"
-          pais="Singapur"
+          nombre="Andrés Eduardo Cruz Quispe"
+          pais="Perú"
           imagen="shawn"
-          cargo="Ingeniero de Software"
+          cargo="Ingeniero de Sistemas"
           empresa="Amazon"
           testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."/>
         <Testimonio
